d238277d8803be66f32a35b272be5cab89d455d0
Muhammad Daniyal <daniyalashraf9790@gmail.com>
2025-12-07 21:59:08 +0500

    Change base URL for Docusaurus configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://danii9790.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/Physical-AI-Humanoid-Robotics-Book/',
+  baseUrl: '/',
   trailingSlash: true,
 
   // GitHub pages deployment config.
